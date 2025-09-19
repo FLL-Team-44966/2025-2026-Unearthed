ef36f6952226a24e821391389b4ce281235adc74
fll-team-44966-student <fllstudent@bethlehem-wels.org>
2025-09-19 16:49:08 -0500

    Update forge.py
    by Elliot

--- forge.py
+++ forge.py
@@ -4,9 +4,11 @@ from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSenso
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
+
 # Clear console at start
 print("\x1b[H\x1b[2J", end="")
 hub = PrimeHub()
+
 # Defines how the ports are used
 left_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
 right_motor = Motor(Port.B)
@@ -14,11 +16,14 @@ front_motor = Motor(Port.C)
 rear_motor = Motor(Port.D)
 left_color_sensor = ColorSensor(Port.E)
 right_color_sensor = ColorSensor(Port.F)
+
 # Initialize the drive base
 # The wheel diameter is 56mm and distance between wheels is 112mm.
 drive_base = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=112)
+
 # Use the gyro to navigate more accurately
 drive_base.use_gyro(True)
+
 ###################################
 # Mission specific code is below:
 
@@ -37,3 +42,4 @@ drive_base.settings(straight_speed=300)
 drive_base.arc(-100, angle=90)
 # Drive forward by 500mm (half a meter)
 drive_base.straight(100)
+
